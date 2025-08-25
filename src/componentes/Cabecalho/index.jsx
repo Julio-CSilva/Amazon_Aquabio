310485b08bfe9430a111bfc9a6b8ea9371a082ef
Gabriel Victor <gvplima09@gmail.com>
2025-08-25 12:18:58 -0300

    cabecalho

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,8 +1,24 @@
 import styled from "styled-components"
-import { Box, Button, ButtonGroup, Divider, Image } from '@chakra-ui/react'
+import {
+    Box,
+    Button,
+    ButtonGroup,
+    Divider,
+    Image,
+    IconButton,
+    Drawer,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    DrawerBody,
+    VStack,
+    Text
+} from '@chakra-ui/react';
+import { useDisclosure } from '@chakra-ui/react';
 import ButtonPersonalizado from "../ButtonPersonalizado";
 import { useLanguage } from "../../componentes/LanguageContext";
 import { motion } from 'framer-motion';
+import { HamburgerIcon } from '@chakra-ui/icons';
 
 const MotionBox = motion.create(Box);
 
@@ -26,6 +42,7 @@ const HeaderEstilizado = styled.header`
 
 const Cabecalho = ({ sectionRefs }) => {
 
+    const { isOpen, onOpen, onClose } = useDisclosure();
     const { language, toggleLanguage } = useLanguage();
 
     const texts = {
@@ -83,6 +100,7 @@ const Cabecalho = ({ sectionRefs }) => {
 
             <Box as='div'
                 ml='10rem'
+                display={{ base: 'none', lg: 'block' }}
             >
                 <ButtonGroup
                     spacing='1rem'
@@ -119,6 +137,68 @@ const Cabecalho = ({ sectionRefs }) => {
                         EN</Button>
                 </ButtonGroup>
             </Box>
+
+            {/* Ícone do menu hambúrguer para mobile - Exibido apenas em telas pequenas */}
+            <Box display={{ base: 'flex', lg: 'none' }} mr="1rem" alignItems="center">
+                <Text color="white" fontWeight="bold">Menu</Text>
+                <IconButton
+                    aria-label="Abrir Menu"
+                    icon={<HamburgerIcon  boxSize={5}/>}
+                    onClick={onOpen}
+                    bg="#4F99A6"
+                    variant="ghost"
+                    colorScheme="whiteAlpha"
+                    color="white"
+                    _hover={{ background: 'rgba(255, 255, 255, 0.1)' }}
+                    _active={{ bg: "#2A5B69" }}
+                    ml={2}
+                />
+            </Box>
+
+            {/* O Drawer (menu lateral) */}
+            <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+                <DrawerOverlay />
+                <DrawerContent backgroundColor="#365B6D" color="white">
+                    <DrawerCloseButton />
+                    <DrawerBody mt="3rem">
+                        <VStack spacing="0.3rem" align="stretch">
+                            <ButtonPersonalizado text={texts[language].inicio} route='/' scrollToSection={scrollToSection} section={'apresentacao'} />
+                            <Divider borderColor="gray.500" />
+                            <ButtonPersonalizado text={texts[language].sobre} route='/' scrollToSection={scrollToSection} section={'definicao'} />
+                            <Divider borderColor="gray.500" />
+                            <ButtonPersonalizado text={texts[language].mapa} route='/' scrollToSection={scrollToSection} section={'mapa'} />
+                            <Divider borderColor="gray.500" />
+                            <ButtonPersonalizado text={texts[language].metodologia} route='/' scrollToSection={scrollToSection} section={'metodologia'} />
+                            <Divider borderColor="gray.500" />
+                            <ButtonPersonalizado text={texts[language].galeria} route='/' scrollToSection={scrollToSection} section={'galeria'} />
+                            <Divider borderColor="gray.500" />
+                            <ButtonPersonalizado text={texts[language].publicacoes} route='/' scrollToSection={scrollToSection} section={'publicacoes'} />
+                            <Divider borderColor="gray.500" />
+                            <ButtonPersonalizado text={texts[language].pesquisadores} route='/' scrollToSection={scrollToSection} section={'pesquisadores'} />
+                            <Divider borderColor="gray.500" />
+                            <ButtonPersonalizado text={texts[language].contato} route='/Contato' scrollToSection={scrollToSection} section={''} />
+                            <Divider borderColor="gray.500" />
+                            <Box textAlign="center" mt="2rem">
+                                <ButtonGroup>
+                                    <Button
+                                        onClick={() => toggleLanguage('pt')}
+                                        colorScheme={language === 'pt' ? 'whiteAlpha' : 'gray'}
+                                        fontWeight={language === 'pt' ? 'bold' : 'normal'}
+                                        variant={language === 'pt' ? 'solid' : 'ghost'}
+                                    >PT-BR</Button>
+                                    <Divider orientation="vertical" />
+                                    <Button
+                                        onClick={() => toggleLanguage('en')}
+                                        colorScheme={language === 'en' ? 'whiteAlpha' : 'gray'}
+                                        fontWeight={language === 'en' ? 'bold' : 'normal'}
+                                        variant={language === 'en' ? 'solid' : 'ghost'}
+                                    >EN</Button>
+                                </ButtonGroup>
+                            </Box>
+                        </VStack>
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
 
         </HeaderEstilizado>
     )
